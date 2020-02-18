27e5d1c97cd1d53cd4049d464fe9252c1ae774d3
Ivan <ivan@ivan.Dlink>
2020-02-18 15:52:14 +0200

    Sets up server & home hbs files

---
+++ src/controllers/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const home = require('./home.js');
+const error = require('./error.js');
+const game = require('./game.js');
+const authenticate = require('./authenticate.js');
+//const middlewares = require('../middlewares/index.js');
+
+router.get('/', /*middlewares.authCheck,*/ home.get);
+router.get('/game', /*middlewares.authCheck,*/ game.get);
+// router.get('/authenticate', authenticate.get);
+// router.post('/addUser', authenticate.addUser);
+// router.get('/logout', authenticate.logout);
+// router.use(error.client);
+// router.use(error.server);
+
+module.exports = router;
